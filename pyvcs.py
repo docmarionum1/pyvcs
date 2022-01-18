d1b2139f496f8b0a8680fcba96de345a18825f36
kevidryon2 <gigetta24@msn.com>
2022-01-18 23:26:38 +0100

    Added error handling to file reader
    When there is no parameter passed to the interpreter, it will just tell the user the usage of the program.

--- pyvcs.py
+++ pyvcs.py
@@ -468,16 +468,17 @@ def main():
 
     with audio_context:
         #dis.dis(open(sys.argv[1]).read())
-
-        exec(open(sys.argv[1]).read(), {
-            "USER_CODE": True,
-            "pyvcs": PYVCS(),
-            #"pyvcs":  Namespace(**globals()),
-            #"pyvcs": PYVCS(),
-            "__name__": ".".join(Path(sys.argv[1].replace(".py", "")).parts),
-            "_pyvcs_display_step": display_step3
-
-        })
+        try:
+            exec(open(sys.argv[1]).read(), {
+                "USER_CODE": True,
+                "pyvcs": PYVCS(),
+                #"pyvcs":  Namespace(**globals()),
+                #"pyvcs": PYVCS(),
+                "__name__": ".".join(Path(sys.argv[1].replace(".py", "")).parts),
+                "_pyvcs_display_step": display_step3
+            })
+        except:
+            print("PyVCS - Created by docmarionum1 and kevidryon2\nUsage: <path-to-pyvcs-python>/python pyvcs.py <file>")
 
         display._quit()
 
